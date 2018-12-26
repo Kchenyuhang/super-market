94490542ef4d15749eb38f20ac44076c4eaefe4f
杨苏祥 <3597107837@qq.com>
2018-12-26 22:09:53 +0800

    更新代码的密钥

--- src/main/java/com/soft1841/sm/until/DAOFactory.java
+++ src/main/java/com/soft1841/sm/until/DAOFactory.java
@@ -50,5 +50,13 @@ public class DAOFactory {
     public static TypeDAO getTypeDAOInstance() {
         return new TypeDAOImpl();
     }
+    public static TicketDAO getTicketDAOInstance(){ return  new TicketDAOImpl();}
+    public static VIPDAO  getVIPDAOInstance(){
+        return new VIPDAOImpl();
+    }
+    public static DetailDAO getDetailDAOInstance(){
+        return new DetailDAOImpl();
+
+    }
 
 }

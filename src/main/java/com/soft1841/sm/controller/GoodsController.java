755a9bd1013b33a74fa7c77c3cd589397e12765d
Moe <1321339159@qq.com>
2018-12-27 09:56:52 +0800

    导出数据

--- src/main/java/com/soft1841/sm/controller/GoodsController.java
+++ src/main/java/com/soft1841/sm/controller/GoodsController.java
@@ -227,7 +227,7 @@ public class GoodsController  implements Initializable{
         ExcelExport.export(goodsList);
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("提示信息");
-        alert.setHeaderText("商品数据已导出!请到F盘根目录查看!");
+        alert.setHeaderText("商品数据已导出!请到D盘根目录查看!");
         alert.showAndWait();
     }
 }

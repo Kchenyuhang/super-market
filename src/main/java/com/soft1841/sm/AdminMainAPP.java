58a82d2703ee6713b61bdd0b51f3f8b49a0680a5
Kchenyuhang <kchenyuhang@gmail.com>
2018-12-28 15:32:57 +0800

    增加新功能（未完）

--- src/main/java/com/soft1841/sm/AdminMainAPP.java
+++ src/main/java/com/soft1841/sm/AdminMainAPP.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -21,6 +22,7 @@ public class AdminMainAPP extends Application {
         Scene scene = new Scene(root);
         primaryStage.setMaximized(true);
         scene.getStylesheets().add(AdminMainAPP.class.getResource("/css/manage.css").toExternalForm());
+        primaryStage.getIcons().add(new Image("/img/star.png"));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
